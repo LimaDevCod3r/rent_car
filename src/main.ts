4459a37ab8521b39ea37827d6e466ff1e1171ec7
LimaDevCod3r <diegolima20100@gmail.com>
2025-03-27 20:30:42 -0300

    feat: habilitar CORS para desenvolvimento local e melhorar o tratamento de erros
    Adiciona a configuração do CORS para permitir requisições de 'http://localhost:5173' com métodos, cabeçalhos e credenciais específicos. Além disso, melhora o tratamento de erros durante a inicialização da aplicação, registrando os erros e finalizando o processo caso a inicialização falhe.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Permite o envio de cookies e cabeçalhos de autenticação
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Falha ao iniciar à aplicação:', error);
+  process.exit(1);
+});
